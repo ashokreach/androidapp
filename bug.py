2261815b1e08ea8b5e5227230c16080ae73116c8
ashokreach <ashok.bnym@gmail.com>
2017-01-10 18:41:36 -0500

    change
    change

--- bug.py
+++ bug.py
@@ -1,6 +1,6 @@
 #im a bug loop
 i=5
-while i==5:#using while loop
+while i==5:#using while loop111
     print('You got a VIRUS program and you cant stop me- LOL')
 input('how is the bug treating you dear')
 
